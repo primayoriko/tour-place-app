6ed1f19d675951a618cd6418b9ac7ab0c017ce10
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-01-19 15:10:14 +0700

    feat: add image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:basic_flutter_app/detail_screen.dart';
+import 'package:basic_flutter_app/main_screen.dart';
 
 void main() => (runApp(MyApp()));
 
@@ -8,86 +10,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: DetailScreen(),
+      // theme: ThemeData(
+      //   // This is the theme of your application.
+      //   //
+      //   // Try running your application with "flutter run". You'll see the
+      //   // application has a blue toolbar. Then, without quitting the app, try
+      //   // changing the primarySwatch below to Colors.green and then invoke
+      //   // "hot reload" (press "r" in the console where you ran "flutter run",
+      //   // or simply save your changes to "hot reload" in a Flutter IDE).
+      //   // Notice that the counter didn't reset back to zero; the application
+      //   // is not restarted.
+      //   primarySwatch: Colors.red,
+      //   // This makes the visual density adapt to the platform that you run
+      //   // the app on. For desktop platforms, the controls will be smaller and
+      //   // closer together (more dense) than on mobile platforms.
+      //   visualDensity: VisualDensity.adaptivePlatformDensity,
+      // ),
+      theme: ThemeData.dark(),
+      // home: DetailScreen(),
+      home: MainScreen(),
       // home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-              child: Text(
-                  'Farm Houses',
-                  textAlign: TextAlign.center,
-                  // textAlign: TextAlignVertical.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold
-                  ),
-              ),
-              margin: EdgeInsets.only(top: 20),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 16),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8),
-                      Text('Open Weekend')
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.access_time),
-                      SizedBox(height: 8),
-                      Text('Open Weekend')
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.monetization_on),
-                      SizedBox(height: 8),
-                      Text('Open Weekend')
-                    ],
-                  ),
-                ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(16),
-            child: Text(
-              'lorem ipsum sir dolor amet.. ... .... ...asdsa ..sad sad.. .asds ad.s',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15)
-            )
-          )
-        ],
-      )
     );
   }
 }
