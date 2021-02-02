4d346339631b74bae37abc77e2ce824c39d6f2fc
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-02-02 10:52:57 +0700

    feat: add content

--- lib/data.dart
+++ lib/data.dart
@@ -11,7 +11,7 @@ var tourismPlaceList = [
     openDays: 'Open Everyday',
     openTime: '09:00 - 20:00',
     ticketPrice: 'Rp 25000',
-    imageAsset: 'assets/images/waterfall.jpeg',
+    imageAsset: 'assets/images/farm_house.png',
     imageUrls: [
       'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
       'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
@@ -21,14 +21,14 @@ var tourismPlaceList = [
     rating: 4,
   ),
   Place(
-    name: 'Monumen Nasional',
-    location: 'Jakarta',
+    name: 'Raja Ampat',
+    location: 'Papua Barat',
     description:
-    'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+    'Kepulauan Raja Ampat merupakan rangkaian empat gugusan pulau yang berdekatan dan berlokasi di barat bagian Kepala Burung Pulau Papua. Secara administrasi, gugusan ini berada di bawah Kabupaten Raja Ampat, Provinsi Papua Barat.',
     openDays: 'Open Everyday',
-    openTime: '09:00 - 20:00',
-    ticketPrice: 'Rp 25000',
-    imageAsset: 'assets/images/waterfall.jpeg',
+    openTime: '06:00 - 22:00',
+    ticketPrice: 'Rp 200000',
+    imageAsset: 'assets/images/raja_ampat.png',
     imageUrls: [
       'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
       'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
@@ -41,10 +41,10 @@ var tourismPlaceList = [
     name: 'Curug Cikaso',
     location: 'Sukabumi',
     description:
-    'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+    'Air Terjun/Curug Cikaso merupakan salah satu air terjun yang terletak di selatan Kabupaten Sukabumi. Air terjun ini juga dikenal sebagai Curug Luhur, tetapi nama Curug Cikaso lebih dikenal masyarakat sekitar dikarenakan aliran airnya berasal dari anak Sungai Cikaso yaitu Sungai Cicurug.',
     openDays: 'Open Everyday',
-    openTime: '09:00 - 20:00',
-    ticketPrice: 'Rp 25000',
+    openTime: '07:00 - 19:00',
+    ticketPrice: 'Rp 3000',
     imageAsset: 'assets/images/waterfall.jpeg',
     imageUrls: [
       'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
@@ -58,11 +58,11 @@ var tourismPlaceList = [
     name: 'Museum Geologi',
     location: 'Bandung',
     description:
-    'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-    openDays: 'Open Everyday',
-    openTime: '09:00 - 20:00',
-    ticketPrice: 'Rp 25000',
-    imageAsset: 'assets/images/waterfall.jpeg',
+    'Museum Geologi didirikan pada tanggal 16 Mei 1929 Museum ini telah direnovasi dengan dana bantuan dari JICA. Setelah mengalami renovasi, Museum Geologi dibuka kembali dan diresmikan oleh Wakil Presiden RI, Megawati Soekarnoputri pada tanggal 23 Agustus 2000.',
+    openDays: 'Weekday',
+    openTime: '08:00 - 18:00',
+    ticketPrice: 'Rp 10000',
+    imageAsset: 'assets/images/museum_geologi.png',
     imageUrls: [
       'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
       'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
